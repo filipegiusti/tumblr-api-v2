2db50c98deab0510f62b9583fa250ee964a11e8e
Filipe Giusti <filipegiusti@gmail.com>
2011-12-12 22:50:35 -0200

    Refactor code.

--- lib/tumblr-api-v2/client.rb
+++ lib/tumblr-api-v2/client.rb
@@ -1,16 +1,31 @@
-require "tumblr-api-v2/apikey_api"
 require "httparty"
 
 module TumblrApiV2
   class TumblrError < StandardError; end
 
   class Client
-    include ApikeyApi
-
     include HTTParty
     base_uri 'http://api.tumblr.com/v2'
 
     attr_reader :consumer_key
+    
+    METHODS = {
+      apikey: {
+        info:  '/info',
+        posts: '/posts'
+      }
+    }
+
+    METHODS.each do |access, methods|
+      methods.each do |method, path|
+        define_method method do |*args|
+          raise ArgumentError if args.size > 2 || args.size == 0
+          blog_url, options = *args
+          options ||= {}
+          get_on_path(blog_url, path, options)
+        end
+      end
+    end
 
     def initialize(consumer_key = nil)
       @consumer_key = consumer_key
@@ -28,6 +43,17 @@ module TumblrApiV2
 
     def has_apikey_access?
       not @consumer_key.nil?
+    end
+
+    def get_on_path(blog_url, path, options = {})
+      raise TumblrError unless has_apikey_access?
+
+      blog_domain = filter_blog_domain(blog_url)
+      get_blog_api("/#{blog_domain}#{path}", query: options.merge(apikey_option))
+    end
+
+    def filter_blog_domain(blog_url)
+      blog_url.gsub(/^https?:\/\//, '').chomp('/')
     end
   end
 end
--- spec/tumblr-api-v2/client_spec.rb
+++ spec/tumblr-api-v2/client_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe TumblrApiV2::Client do
+  use_vcr_cassette
+
   it "should instance a Client" do
     TumblrApiV2::Client.new.should be_a(TumblrApiV2::Client)
   end
@@ -8,6 +10,32 @@ describe TumblrApiV2::Client do
   it "should instance a Client with a consumer key" do
     c = TumblrApiV2::Client.new('fake-consumer-key')
     c.consumer_key.should == 'fake-consumer-key'
+  end
+  
+  it "without a consumer key should raise an exception" do
+    expect do
+      TumblrApiV2::Client.new.info('http://fgiusti-test-data.tumblr.com/')
+    end.to raise_error(TumblrApiV2::TumblrError)
+  end
+
+  describe "with a valid consumer key" do
+    before(:each) do
+      @client = TumblrApiV2::Client.new(CONSUMER_KEY)
+    end
+
+    describe "#info" do
+      it "should get blog's info" do
+        info = @client.info('http://fgiusti-test-data.tumblr.com/')
+        info['response']['blog']['title'].should  == 'Cool blog.'
+      end
+    end
+
+    describe "#posts" do
+      it "should get blog's posts" do
+        info = @client.posts('http://fgiusti-test-data.tumblr.com/')
+        info['response']['posts'].last['post_url'].should  == 'http://fgiusti-test-data.tumblr.com/post/6140355204/thats-a-test-post'
+      end
+    end
   end
 end
 
